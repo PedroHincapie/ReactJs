8241936c6efc6cc60761f6010eb9125a116b158b
INTERGRUPO\Phincapie <Phincapie@intergrupo.com>
2018-02-08 20:26:12 -0500

    Crear archivos index de la apliacion ReactJs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import React from'react';
+import ReactDOM from'react-dom';
+// Importamos dependencias de React
 
+const app = document.getElementById('app');
+// Referenciamos un elemento con ID app
+
+const holaMundo = <h1>Hola Mundo</h1>;
+// Insertamos en una constante lo que queremos renderizar
+
+ReactDOM.render(holaMundo, app);
+// Le decimos a ReactDOM que renderizar y donde renderizar
+// Que renderizar : Puede ser un elemento o un componte de react
+// Donde : debe ser un lugar en el Dom que ya exista
